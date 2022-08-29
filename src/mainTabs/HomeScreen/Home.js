c72d4630786136af8c8a350a609b2096cfc56dc5
andalib-oper <andalibquraishi14@gmail.com>
2022-08-29 08:18:11 +0530

    updated

--- src/mainTabs/HomeScreen/Home.js
+++ src/mainTabs/HomeScreen/Home.js
@@ -1,14 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient';
+import Carousel from 'react-native-snap-carousel';
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <LinearGradient colors={['#C2F1FF', '#F5F5F5']} style={styles.linearGradient}>
+      <Text>Welcome</Text>
+      </LinearGradient>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  linearGradient: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+})
